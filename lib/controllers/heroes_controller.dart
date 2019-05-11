9f786112a4180665ccee14b286014c3618c03ed8
Obinna Igwe <igwenus619@gmail.com>
2019-05-11 11:48:44 +0100

    Load data from postgresql

--- lib/controllers/heroes_controller.dart
+++ lib/controllers/heroes_controller.dart
@@ -2,12 +2,12 @@ import 'dart:async';
 
 import 'package:aqueduct/aqueduct.dart';
 
+import 'package:first_api/models/hero.dart';
+
 class HeroesController extends ResourceController {
   HeroesController(this.context);
 
-
   ManagedContext context;
-
 
   final _heroes = [
     {'id': 11, 'name': 'Captain America'},
@@ -17,22 +17,38 @@ class HeroesController extends ResourceController {
     {'id': 15, 'name': 'Hawkeye'}
   ];
 
+  // Refer to the aqueduct docs tutorial
   @Operation.get()
   Future<Response> getAllHeroes() async {
-    return Response.ok(_heroes);
+    // Create a query to be sent to the database
+    final heroQuery = Query<Hero>(context);
+    // Get all the instance types (in this case, heroes) from the database
+//    final heroes = heroQuery.fetch();
+//    return Response.ok(heroes);
+
+    /// Must await the value of fetch or else the error below is thrown
+    ///[Converting object to an encodable object failed: Instance of 'Future<List<Hero>>']
+
+    final heroes = await heroQuery.fetch();
+    return Response.ok(heroes);
   }
 
   @Operation.get('id')
-  Future<Response> getHeroByID(@Bind.path('id') int heroID) async {
-    //final id = int.parse(request.path.variables['id']);
-    // Query the heroes map. If there is a match, return the result
-    // Else, return null
-    final hero = _heroes.firstWhere((hero) {return hero['id'] == heroID;}, orElse: () {return null;},);
-    // Throw HTTP 404 error if no resource matches the query
+  Future<Response> getHeroByID(@Bind.path('id') int heroId) async {
+    // This query is a filter that filters the the contents of the
+    //heroes table with the condition that the row index of the specific
+    // hero must match the path variable
+    final heroQuery = Query<Hero>(context)
+      ..where((hero) {
+        return hero.id;
+      }).equalTo(heroId);
+
+    // Return one hero from the table that fulfils the query condition
+    final hero = await heroQuery.fetchOne();
+
     if (hero == null) {
       return Response.notFound();
     }
-
     return Response.ok(hero);
   }
 }
